5810b7cdcf55c19de2ff37f02d19502a15788e87
Gbenga <olugbengaojo97@gmail.com>
2021-08-02 01:17:20 -0500

    commit file

---
+++ script.js
@@ -0,0 +1,35 @@
+var weather = {
+    apiKey: "994af978bc4e972b5d2b55a5b7d5e214",
+    getWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            +city 
+            +"&units=imperial&appid="
+            +this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    display: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp").innerText = "Temp:" + temp + "F";
+        document.querySelector(".wind").innerText = "Wind Speed:" + speed + "km/h";
+        document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
+    },
+    search: function () {
+        this.getWeather(document.querySelector(".search-button").value);
+    }
+};
+
+document
+    .querySelector(".search-button")
+    .addEventListener("click", function () {
+        weather.search();
+    });
